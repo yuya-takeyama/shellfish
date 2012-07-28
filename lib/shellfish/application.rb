94f826b961aa0aa3c58771e9ad0ad63b05afddb3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-07-28 15:31:35 +0900

    Refactoring

--- lib/shellfish/application.rb
+++ lib/shellfish/application.rb
@@ -3,6 +3,10 @@ require 'rspec/expectations/differ'
 require 'readline'
 
 module Shellfish
+  class QuitException < ::Exception; end
+  class NextProblemException < ::Exception; end
+  class SkipEvaluationException < ::Exception; end
+
   class Application
     def initialize(argv)
       @argv   = argv
@@ -15,29 +19,52 @@ module Shellfish
       @argv.each do |file|
         try_problem @loader.load(file)
       end
+    rescue QuitException
+      puts "Quitting shellfish..."
+    ensure
       puts "Good bye."
     end
 
     def try_problem(problem)
       show_problem(problem)
-      while buf = ::Readline.readline('shellfish $ ', true)
-        break if buf =~ /^\s*:?exit\s*$/
-        if buf =~ /^\s*:show\s*$/
-          show_problem
-          next
-        end
-        got = `#{buf}`
-        if problem.expected_result != got
-          puts "NG"
-          show_string @differ.diff_as_string(got, problem.expected_result)
+      while input = ::Readline.readline('shellfish $ ', true)
+        begin
+        on_quit if input =~ /^\s*(?::?exit|:quit)\s*$/
+        on_show_problem(problem) if input =~ /^\s*:show\s*$/
+
+        output = `#{input}`
+        if problem.expected_result != output
+          on_ng problem, input, output
         else
-          puts "OK"
-          show_string got
-          puts "Congratulations!"
-          break
+          on_ok problem, input, output
         end
         puts
+        rescue NextProblemException
+          break
+        rescue SkipEvaluationException
+        end
       end
+    end
+
+    def on_ok(problem, input, output)
+      puts "OK"
+      show_string output
+      puts "Congratulations!"
+      raise NextProblemException
+    end
+
+    def on_ng(problem, input, output)
+      puts "NG"
+      show_string @differ.diff_as_string(output, problem.expected_result)
+    end
+
+    def on_quit
+      raise QuitException
+    end
+
+    def on_show_problem(problem)
+      show_problem problem
+      raise SkipEvaluationException
     end
 
     def show_problem(problem)
